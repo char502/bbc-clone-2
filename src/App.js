f07e26c5d608ef42d112dfdf5a551661c410f06a
char502 <char502@hotmail.com>
2019-09-12 21:44:32 +0100

    Main body News one container layout almost complete

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import bellImage from "./Images/bellImage.png";
 // =============== First Nav ===============
 const NavContainer = styled.div`
   width: 100vw;
-  height: 50px;
+  height: 40px;
   // background: green;
 `;
 
@@ -38,7 +38,7 @@ const LogoContainer = styled.div`
 `;
 
 const BBCLogoSizing = styled.img`
-  width: 100px;
+  width: 85px;
   height: 24px;
 `;
 
@@ -51,24 +51,29 @@ const Logo = () => (
 //=============== Sign In ===============
 const SignInContainer = styled.div`
   flex: 1;
-  height: 50px;
+  height: 40px;
   // background: yellow;
   padding: 0 10px;
   display: flex;
   align-items: center;
+  margin-right: 5px;
+  // flex-wrap: nowrap;
+  border: 0.5px solid lightgrey;
+  border-top: 0;
+  border-bottom: 0;
 `;
 
 const UserIconSizing = styled.img`
-  width: 25px;
   height: 20px;
   padding-right: 10px;
 `;
 
 const SignIn = styled.span`
-  font-size: 16px;
+  font-size: 14px;
   text-decoration: none;
+  margin-bottom: 20px;
   // display: flex;
-  // flex-wrap: none;
+  // flex-wrap: inherit;
 `;
 
 const UserLogo = () => <UserIconSizing src={userIcon} />;
@@ -86,26 +91,45 @@ const SignInComponent = () => (
 
 //=============== Page Links ===============
 const PageLinksContainer = styled.div`
-  height: 50px;
+  height: 40px;
   // background: blue;
   display: flex;
+  // margin: 0 auto;
+  // padding: 0 16px;
   flex: 3;
   align-items: center;
+  margin-right: 65px;
 `;
 
 const Link = styled.div`
-  padding: 0 15px;
-  font-size: 16px;
-  color: black;
-  border-right: 1px solid black;
+  // padding: 0 15px;
+  padding: 10px 15px 10px 15px;
+  font-size: 14px;
+  font-weight: bold;
+  color: #505050;
+  border-right: solid lightgrey;
+  border-width 0 0.5px;
   display: flex;
   align-items: center;
+  
+`;
+
+const LinkMore = styled.div`
+  padding: 10px 15px 10px 15px;
+  // margin-right: 10px;
+  color: #505050;
+  border: solid lightgrey;
+  font-size: 14px;
+  font-weight: bold;
+  border-width 0 0.5px 0 0;
+  
 `;
 
 const BellImageSizing = styled.img`
   width: 30px;
   height: 25px;
-  margin-top: 5px;
+  margin: -3px;
+  // margin-top: 5px;
 `;
 
 const BellImageComponent = () => <BellImageSizing src={bellImage} />;
@@ -115,36 +139,41 @@ const Links = () => (
     <Link>
       <BellImageComponent />
     </Link>
+    <Link>Home</Link>
     <Link>News</Link>
     <Link>Sport</Link>
     <Link>Weather</Link>
     <Link>iPlayer</Link>
     <Link>Sounds</Link>
     <Link>CBBC</Link>
-    <Link>More</Link>
+    <LinkMore>More</LinkMore>
   </PageLinksContainer>
 );
 
 //=============== Search Bar ===============
 const SearchBarContainer = styled.div`
   flex: 1;
-  height: 50px;
+  height: 40px;
   // background: yellow;
   padding: 0 10px;
   display: flex;
   align-items: center;
+  border: solid lightgrey;
+  border-width 0 0 0 0.5px;
 `;
 
 const SearchBarInput = styled.input`
   color: black;
   background-color: #e4e4e4;
+  border: solid white;
+  padding: 5px;
 `;
 
 const SearchBarForm = () => {
   return (
     <div>
       <form action="Submit">
-        <input type="text" placeholder="Search" />
+        <SearchBarInput type="text" placeholder="Search" />
         {/* <button>Img</button> */}
       </form>
     </div>
@@ -168,7 +197,7 @@ const Nav = () => (
   </NavContainer>
 );
 
-//=============== Nav News Container ===============
+//=============== Nav News ===============
 const NavNewsContainer = styled.div`
   width: 100vw;
   height: 128px;
@@ -180,7 +209,7 @@ const NavNewsInner = styled.div`
   height: 100%;
   max-width: 1200px;
   margin: 0 auto;
-  padding: 0 16px;
+  // padding: 0 16px;
   // display: flex;
   justify-content: space-between;
   align-items: center;
@@ -202,12 +231,18 @@ const NewsLinksContainer = styled.div`
 `;
 
 const NewsLink = styled.div`
-  padding: 0 15px;
-  font-size: 16px;
+  padding: 0 10px;
+  font-size: 14px;
   color: white;
-  border-right: 1px solid #ffffff;
+  border-right: 0.5px solid rgba(211, 211, 211, 0.7);
   display: flex;
   align-items: center;
+  &:first-child {
+    padding-left: 0;
+  }
+  &:last-child {
+    border-width: 0;
+  }
 `;
 
 const NewsLinks = () => (
@@ -223,6 +258,9 @@ const NewsLinks = () => (
     <NewsLink>Family & Education</NewsLink>
     <NewsLink>Entertainment & Arts</NewsLink>
     <NewsLink>Stories</NewsLink>
+    <NewsLink>Video & Audio</NewsLink>
+    <NewsLink>In Pictures</NewsLink>
+    <NewsLink>Newsbeat</NewsLink>
     <NewsLink>More</NewsLink>
   </NewsLinksContainer>
 );
@@ -233,10 +271,245 @@ const NavNews = () => (
       <div>
         <NewsHeader>NEWS</NewsHeader>
       </div>
-
       <NewsLinks />
     </NavNewsInner>
   </NavNewsContainer>
+);
+
+//=============== Nav News Local ===============
+const NavNewsLocalContainer = styled.div`
+  width: 100vw;
+  height: 44px;
+`;
+
+const NavNewsLocalInner = styled.div`
+  width: 100%;
+  height: 100%;
+  max-width: 1200px;
+  margin: 0 auto;
+  // padding: 0 16px;
+  // display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const NewsLocalLink = styled.div`
+  padding: 0 15px;
+  font-size: 16px;
+  color: black;
+  border-right: 0.5px solid lightgrey;
+  display: flex;
+  align-items: center;
+  &:first-child {
+    padding-left: 0;
+  }
+  &:last-child {
+    border-width: 0;
+  }
+`;
+
+const NewsLocalLinks = () => (
+  <NewsLinksContainer>
+    <NewsLocalLink>England</NewsLocalLink>
+    <NewsLocalLink>N. Ireland</NewsLocalLink>
+    <NewsLocalLink>Scotland</NewsLocalLink>
+    <NewsLocalLink>Alba</NewsLocalLink>
+    <NewsLocalLink>Wales</NewsLocalLink>
+    <NewsLocalLink>Cymru</NewsLocalLink>
+    <NewsLocalLink>Local News</NewsLocalLink>
+  </NewsLinksContainer>
+);
+
+const NavNewsLocal = () => (
+  <NavNewsLocalContainer>
+    <NavNewsLocalInner>
+      <NewsLocalLinks />
+    </NavNewsLocalInner>
+  </NavNewsLocalContainer>
+);
+
+//=============== Main Body News One ===============
+const MainBodyNewsOneContainer = styled.div`
+  width: 100vw;
+  max-width: 1200px;
+  // height: 1000px
+  height: auto;
+  margin: 0 auto;
+  display: flex;
+  // justify-content: space-between;
+`;
+
+const MainHeadlinesStyle = styled.div`
+  // padding-left: 15px:
+  flex: 4;
+  display: flex;
+  flex-direction: column;
+  border: 0.5px solid black;
+`;
+
+// const HeadLineItem = styled.div`
+//   padding-left: 20px;
+// `;
+
+const MainHeadlinesOneTopBanner = styled.div`
+  margin: 12px;
+  background: green;
+  border: 0.5px solid black;
+  // height: 300px;
+  flex: 1;
+  display: flex;
+`;
+
+const TopHeadlineLeft = styled.div`
+  padding: 10px;
+  flex: 3;
+`;
+
+const TopHeadLineRight = styled.div`
+  padding: 10px;
+  flex 1;
+`;
+
+const MainHeadlinesOneBottomBanner = styled.div`
+  padding: 10px;
+  margin: 12px;
+  background: yellow;
+  border: 0.5px solid black;
+  // height: 300px;
+  flex: 1;
+  display: flex;
+`;
+
+const NewsItemImage = styled.image`
+  width: 100%
+  height: 10px;
+  background: LightGray;
+
+`;
+
+const NewsItem = styled.div`
+  padding: 5px;
+  width: calc(25% - 10px);
+`;
+
+const NewsItemContainer = () => (
+  <NewsItem>
+    <NewsItemImage />
+    <h5>News Headline</h5>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Enim ut sem viverra
+      aliquet eget. Sed risus ultricies tristique nulla aliquet enim.
+    </p>
+  </NewsItem>
+);
+
+const MainHeadlines = () => (
+  <MainHeadlinesStyle>
+    <MainHeadlinesOneTopBanner>
+      <TopHeadlineLeft>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim ut
+          sem viverra aliquet eget. Sed risus ultricies tristique nulla aliquet
+          enim. Eu scelerisque felis imperdiet proin. Dolor sit amet consectetur
+          adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing
+          elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+          aliqua. Enim ut sem viverra aliquet eget. Sed risus ultricies
+          tristique nulla aliquet enim. Eu scelerisque felis imperdiet proin.
+          Dolor sit amet consectetur adipiscing elit. Lorem ipsum dolor sit
+          amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+          labore et dolore magna aliqua. Enim ut sem viverra aliquet eget. Sed
+          risus ultricies tristique nulla aliquet enim. Eu scelerisque felis
+          imperdiet proin. Dolor sit amet consectetur adipiscing elit.
+        </p>
+      </TopHeadlineLeft>
+      <TopHeadLineRight>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Enim ut sem viverra
+        aliquet eget. Sed risus ultricies tristique nulla aliquet enim. Eu
+        scelerisque felis imperdiet proin. Dolor sit amet consectetur adipiscing
+        elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim ut sem
+        viverra aliquet eget.
+      </TopHeadLineRight>
+      {/* <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Enim ut sem viverra
+        aliquet eget. Sed risus ultricies tristique nulla aliquet enim. Eu
+        scelerisque felis imperdiet proin. Dolor sit amet consectetur adipiscing
+        elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim ut sem
+        viverra aliquet eget. Sed risus ultricies tristique nulla aliquet enim.
+        Eu scelerisque felis imperdiet proin. Dolor sit amet consectetur
+        adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing
+        elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Enim ut sem viverra aliquet eget. Sed risus ultricies tristique nulla
+        aliquet enim. Eu scelerisque felis imperdiet proin. Dolor sit amet
+        consectetur adipiscing elit.
+      </p> */}
+    </MainHeadlinesOneTopBanner>
+    <MainHeadlinesOneBottomBanner>
+      <NewsItemContainer />
+      <NewsItemContainer />
+      <NewsItemContainer />
+      <NewsItemContainer />
+    </MainHeadlinesOneBottomBanner>
+  </MainHeadlinesStyle>
+);
+
+const SideBarStyle = styled.div`
+  // margin: 24px;
+  display: flex;
+  flex: 1;
+  bakcground: red;
+  flex-direction: column;
+  border: 0.5px solid black;
+`;
+
+const SideBarNewsItem = styled.div`
+  padding-left: 24px;
+  border: 0.5px solid black;
+`;
+
+const SideBar = () => (
+  <SideBarStyle>
+    <SideBarNewsItem>
+      <h5>Side News Item 1</h5>
+      <p>Lorem ipsum dolor sit amet</p>
+    </SideBarNewsItem>
+    <SideBarNewsItem>
+      <h5>Side News Item 2</h5>
+      <p>Lorem ipsum dolor sit amet</p>
+    </SideBarNewsItem>
+    <SideBarNewsItem>
+      <h5>Side News Item 3</h5>
+      <p>Lorem ipsum dolor sit amet</p>
+    </SideBarNewsItem>
+    <SideBarNewsItem>
+      <h5>Side News Item 4</h5>
+      <p>Lorem ipsum dolor sit amet</p>
+    </SideBarNewsItem>
+    <SideBarNewsItem>
+      <h5>Side News Item 5</h5>
+      <p>Lorem ipsum dolor sit amet</p>
+    </SideBarNewsItem>
+    <SideBarNewsItem>
+      <h5>Side News Item 6</h5>
+      <p>Lorem ipsum dolor sit amet</p>
+    </SideBarNewsItem>
+    <SideBarNewsItem>
+      <h5>Side News Item 7</h5>
+      <p>Lorem ipsum dolor sit amet</p>
+    </SideBarNewsItem>
+  </SideBarStyle>
+);
+
+const MainBodyNewsOne = () => (
+  <MainBodyNewsOneContainer>
+    <MainHeadlines />
+    <SideBar />
+  </MainBodyNewsOneContainer>
 );
 
 //=============== Main App ===============
@@ -245,6 +518,8 @@ function App() {
     <div className="App">
       <Nav />
       <NavNews />
+      <NavNewsLocal />
+      <MainBodyNewsOne />
     </div>
   );
 }
